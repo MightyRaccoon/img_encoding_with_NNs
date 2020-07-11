a7d855a0f47b4ef06d2d6e04068213a4a04207e5
Mikhail Molovtsev <mishamolovtsev@gmail.com>
2020-07-12 00:47:19 +0300

    Metrics with tests V0

--- simpe_convolutional_autoencoder.py
+++ simpe_convolutional_autoencoder.py
@@ -5,13 +5,14 @@ import os
 import click
 import tensorflow as tf
 from tensorflow.keras import models, losses, optimizers, metrics
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Input, Conv2DTranspose, UpSampling2D, LeakyReLU
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Input, Conv2DTranspose, UpSampling2D, LeakyReLU, BatchNormalization
 from tensorflow.keras.utils import plot_model
 
 from utils.DataGenerator import DataGenerator
+from utils.metrics import MAPEavg
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+tf.compat.v1.executing_eagerly()
 
 log = logging.getLogger(__name__)
 
@@ -34,30 +35,33 @@ def main(processed_data_dir, epochs_count, learning_rate, alpha):
     log_dir = 'logs/fit/' + datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
     tf_board_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    data_generator = ImageDataGenerator()
-
     train_dir = processed_data_dir + '/train_set'
     val_dir = processed_data_dir + '/val_set'
     test_dir = processed_data_dir + '/test_set'
 
     train_generator = DataGenerator(
-        data_dir=val_dir,
-        batch_size=8
+        data_dir=train_dir,
+        batch_size=8,
+        n_channels=1
     )
 
     val_generator = DataGenerator(
-        data_dir=train_dir,
-        batch_size=8
+        data_dir=val_dir,
+        batch_size=8,
+        shuffle=False,
+        n_channels=1
     )
 
     log.info('Models configure')
     #Encoder
-    encoding_input = Input(shape=(512, 512, 3))
+    encoding_input = Input(shape=(512, 512, 1))
     encoding_x = Conv2D(filters=128, kernel_size=(3, 3), strides=(2, 2), padding='same')(encoding_input)
     encoding_x = LeakyReLU(alpha=alpha)(encoding_x)
     encoding_x = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='same')(encoding_x)
+    encoding_x = BatchNormalization()(encoding_x)
     encoding_x = Conv2D(filters=64, kernel_size=(3, 3), strides=(2, 2), padding='same')(encoding_x)
     encoding_x = LeakyReLU(alpha=alpha)(encoding_x)
+    encoding_x = BatchNormalization()(encoding_x)
     encoding_x = Conv2D(filters=1, kernel_size=(3, 3), strides=(2, 2), padding='same')(encoding_x)
     encoding_output = LeakyReLU(alpha=alpha)(encoding_x)
 
@@ -65,11 +69,13 @@ def main(processed_data_dir, epochs_count, learning_rate, alpha):
     #Decoder
     decoding_x = Conv2DTranspose(filters=64, kernel_size=(3, 3), strides=(2, 2), padding='same')(encoding_output)
     decoding_x = LeakyReLU(alpha=alpha)(decoding_x)
+    decoding_x = BatchNormalization()(decoding_x)
     decoding_x = UpSampling2D(size=(2, 2))(decoding_x)
-    decoding_x = Conv2DTranspose(filters=126, kernel_size=(3, 3), strides=(2, 2), padding='same')(decoding_x)
+    decoding_x = Conv2DTranspose(filters=128, kernel_size=(3, 3), strides=(2, 2), padding='same')(decoding_x)
     decoding_x = LeakyReLU(alpha=alpha)(decoding_x)
     decoding_x = UpSampling2D(size=(2, 2))(decoding_x)
-    decoding_x = Conv2DTranspose(filters=3, kernel_size=(3, 3), strides=(1, 1), padding='same')(decoding_x)
+    decoding_x = BatchNormalization()(decoding_x)
+    decoding_x = Conv2DTranspose(filters=1, kernel_size=(3, 3), strides=(1, 1), padding='same')(decoding_x)
     decoding_output = LeakyReLU(alpha=alpha)(decoding_x)
 
     #Models
@@ -80,10 +86,17 @@ def main(processed_data_dir, epochs_count, learning_rate, alpha):
     plot_model(encoder, show_shapes=True, to_file='Encoder_model.png')
 
     log.info('Model compiling')
+    loss_and_metrics = [
+        losses.MeanAbsoluteError(),
+        metrics.RootMeanSquaredError(),
+        metrics.MeanAbsolutePercentageError(),
+        metrics.CosineSimilarity(),
+        MAPEavg(batch_shape=[8, 512, 512])
+    ]
     full_model.compile(
         optimizer=optimizers.Adam(learning_rate=learning_rate),
-        loss=losses.MeanAbsoluteError(),
-        metrics=[metrics.MeanAbsoluteError(), metrics.RootMeanSquaredError()]
+        loss=loss_and_metrics[0],
+        metrics=[loss_and_metrics[1:]]
     )
 
     print(tf.keras.backend.shape(decoding_output))
@@ -99,11 +112,16 @@ def main(processed_data_dir, epochs_count, learning_rate, alpha):
     log.info('Model testing')
     test_generator = DataGenerator(
         data_dir=test_dir,
-        batch_size=8
+        batch_size=8,
+        shuffle=False,
+        n_channels=1
     )
 
     model_test_results = full_model.evaluate(x=test_generator)
-    log.info(model_test_results)
+    test_res = {}
+    for metric, value in zip(loss_and_metrics, model_test_results):
+        test_res.update({metric.name: value})
+    log.info(test_res)
 
 
 if __name__ == '__main__':
---
+++ tests/test_metrics.py
@@ -0,0 +1,85 @@
+import sys
+sys.path.append('.')
+
+import tensorflow as tf
+
+from utils.metrics import MAPEavg
+
+tf.compat.v1.disable_eager_execution()
+
+def test_MAPEavg_1d_ident():
+
+    x = tf.Variable([1, 2, 3])
+    y = tf.Variable([1, 2, 3])
+
+    metric = MAPEavg([1, 3])
+    metric.update_state(x, y)
+    res = metric.result()
+
+    tf.assert_equal(res, 0.0)
+
+def test_MAPEavg_1d_pos():
+
+    x = tf.Variable([1, 1, 1])
+    y = tf.Variable([2, 2, 2])
+
+    metric = MAPEavg([1, 3])
+    metric.update_state(x, y)
+    res = metric.result()
+
+    tf.assert_equal(res, 1.0)
+
+def test_MAPEavg_1d_neg():
+
+    x = tf.Variable([2, 2, 2])
+    y = tf.Variable([1, 1, 1])
+
+    metric = MAPEavg([1, 3])
+    metric.update_state(x, y)
+    res = metric.result()
+
+    tf.assert_equal(res, 0.5)
+
+def test_MAPEavg_2d_ident():
+
+    x = tf.Variable([[1, 2, 3], [1, 2, 3]])
+    y = tf.Variable([[1, 2, 3], [1, 2, 3]])
+
+    metric = MAPEavg([2, 3])
+    metric.update_state(x, y)
+    res = metric.result()
+
+    tf.assert_equal(res, 0.0)
+
+def test_MAPEavg_2d_pos():
+
+    x = tf.Variable([[1, 1, 1], [1, 1, 1]])
+    y = tf.Variable([[2, 2, 2], [2, 2, 2]])
+
+    metric = MAPEavg([2, 3])
+    metric.update_state(x, y)
+    res = metric.result()
+
+    tf.assert_equal(res, 1.0)
+
+def test_MAPEavg_2d_neg():
+
+    x = tf.Variable([[2, 2, 2], [2, 2, 2]])
+    y = tf.Variable([[1, 1, 1], [1, 1, 1]])
+
+    metric = MAPEavg([2, 3])
+    metric.update_state(x, y)
+    res = metric.result()
+
+    tf.assert_equal(res, 0.5)
+
+def test_MAPEavg_2d_pos_neg():
+
+    x = tf.Variable([[1, 1, 1], [2, 2, 2]])
+    y = tf.Variable([[2, 2, 2], [1, 1, 1]])
+
+    metric = MAPEavg([2, 3])
+    metric.update_state(x, y)
+    res = metric.result()
+
+    tf.assert_equal(res, 0.75)
